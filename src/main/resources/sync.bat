3cc14b5cb08fd2789c1f8bff1a1a85099ef8dff8
spright <chia7712@gmail.com>
2015-02-26 17:13:39 +0800

    Adding resource

---
+++ src/main/resources/sync.bat
@@ -0,0 +1,6 @@
+copy PlayerFrame.jar \\192.168.100.1\\d\\PlayerFrame2.jar
+copy PlayerFrame.jar \\192.168.100.2\\d\\PlayerFrame2.jar
+copy PlayerFrame.jar \\192.168.100.3\\d\\PlayerFrame2.jar
+copy PlayerFrame.jar \\192.168.100.4\\d\\PlayerFrame2.jar
+copy ControlFrame.jar \\192.168.100.38\\d\\ControlFrame2.jar
+PAUSE
